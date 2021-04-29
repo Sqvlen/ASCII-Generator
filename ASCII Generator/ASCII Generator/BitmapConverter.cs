9c41400d5c58788ca67b21ffebea6b8e53886d87
Kirill <65315887+Sqvlen@users.noreply.github.com>
2021-04-29 23:27:51 +0300

    ASCII-graphics Generator
    ASCII-graphics Generator

---
+++ ASCII Generator/ASCII Generator/BitmapConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ASII_Generator
+{
+    public class BitmapConverter
+    {
+        private readonly Bitmap _bitmap;
+        private readonly char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+        public BitmapConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            var resualt = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                resualt[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, _asciiTable.Length - 1);
+                    resualt[y][x] = _asciiTable[mapIndex];
+                }
+            }
+
+            return resualt;
+        }
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
